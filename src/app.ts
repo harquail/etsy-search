f4170910f90bf667e2bf0a6bfe8164edd4b365af
harquail <nook@harquail.com>
2018-06-17 21:06:47 -0400

    adds search

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,30 @@
 import { Network } from "./network";
 
-const listings = Network.getListings();
-listings.then((a) => {
+const searchElement = document.createElement("input");
+searchElement.setAttribute("type", "search");
+searchElement.setAttribute("id", "search");
+searchElement.addEventListener("input", () => {
+  const query = searchElement.value.match(/\S+/g);
+  const listings2 = Network.getListings(query);
 
-  let listingsHTML = "";
-  for (const listing of a.results) {
-    console.log(listing);
-    if (listing.error_messages) {
-      continue;
+  listings2.then((b) => {
+    let listingsHTML = "";
+    for (const listing of b.results) {
+      if (listing.error_messages) {
+        continue; // skip listings that have errors; they won't have the required fields
+      }
+      listingsHTML += `<div class='listing'>
+      <a href=${listing.url} target=_blank><div class='title'>${listing.title}</div>
+      <img src='${listing.Images[0].url_170x135}'></img>
+      <div class='price'>$${listing.price}</div>
+      </a></div>`; // TODO: localize price
     }
-    listingsHTML += `<div class='listing'>
-    <a href=${listing.url} target=_blank><div class='title'>${listing.title}</div>
-    <img src='${listing.Images[0].url_170x135}'></img>
-    <div class='price'>$${listing.price}</div>
-    </a></div>`; // TODO: localize price
-    console.log(listingsHTML);
-  }
-  document.body.innerHTML = listingsHTML;
-});
-listings.catch((e) => {
-  document.body.innerHTML = `error: ${e}`;
-});
+
+    listingsElement.innerHTML = listingsHTML;
+  });
+}, true);
+
+const listingsElement = document.createElement("div");
+listingsElement.setAttribute("id", "listings");
+document.body.appendChild(searchElement);
+document.body.appendChild(listingsElement);
---
+++ src/network.ts
@@ -0,0 +1,42 @@
+import { Promise } from "es6-promise";
+import { Credentials } from "./credentials";
+
+const listingEndpoint = `https://openapi.etsy.com/v2/listings/active.js`;
+const listingsFields = ["title", "description", "price", "url"];
+const listingsIncludes = "Images:1:0";
+const listingsSort = "score";
+(window as any).loadJSONP = {
+  counter: 0,
+  ownedBy: "Network.ts",
+};
+
+export class Network {
+
+  public static getListings(keywords?: string[]): Promise<any> {
+    return Network.loadJSONP(`${listingEndpoint}?api_key=${Credentials.etsyKey}\
+&fields=${listingsFields.join(",")}\
+&sort_on=${listingsSort}\
+&includes=${listingsIncludes}\
+${keywords ? "&keywords=" + keywords.join(",") : ""}`); // add keywords if they exist
+  }
+
+  private static callbackCounter = 0;
+
+  private static loadJSONP(url, parameter = "callback") {
+    const prop = "loadJSONP.back" + ++Network.callbackCounter;
+    const script = document.createElement("script");
+    function withCleanUp(resolve) {
+      return (result) => {
+        (window as any).loadJSONP[prop] = null;
+        document.head.removeChild(script);
+        resolve(result);
+      };
+    }
+    return new Promise((resolve, reject) => {
+      (window as any).loadJSONP[`back${Network.callbackCounter}`] = withCleanUp(resolve);
+      script.onerror = withCleanUp(reject);
+      script.src = url + "&" + parameter + "=" + prop;
+      document.head.appendChild(script);
+    });
+  }
+}
